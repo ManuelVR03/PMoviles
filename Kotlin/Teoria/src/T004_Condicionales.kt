03ac22c93ec1bfb5b9af8e0aa0c8ffc034b7d524
ManuelVR03 <manuel.verdejo.ramirez.alu@iesjulioverne.es>
2024-10-29 04:59:46 +0100

    Maanual

---
+++ Kotlin/Teoria/src/T004_Condicionales.kt
@@ -0,0 +1,9 @@
+// If y Else
+
+fun main() {
+    val sueldo = 1487.78f
+    if (sueldo > 1100)
+        println("Date con un canto en los dientes")
+    else
+        println("A limpiar calles")
+}
